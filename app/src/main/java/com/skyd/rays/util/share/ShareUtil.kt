a8146875ccf68ad5988906936782ffda8efc0d0b
aaa1115910 <aaa1115910@gmail.com>
2023-11-19 21:22:00 +0800

    [feature] Support sharesheet image preview and custom actions

--- app/src/main/java/com/skyd/rays/util/share/ShareUtil.kt
+++ app/src/main/java/com/skyd/rays/util/share/ShareUtil.kt
@@ -1,13 +1,21 @@
 package com.skyd.rays.util.share
 
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.graphics.drawable.Icon
 import android.net.Uri
+import android.os.Build
+import android.service.chooser.ChooserAction
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.ShareCompat
 import com.skyd.rays.R
 import com.skyd.rays.appContext
+import com.skyd.rays.model.bean.ShareSheetAction
+import com.skyd.rays.model.broadcast.ShareSheetActionsReceiver
 import com.skyd.rays.util.share.app.DiscordAppInfo
 import com.skyd.rays.util.share.app.IAppInfo
 import com.skyd.rays.util.share.app.InstagramAppInfo
@@ -64,28 +72,47 @@ object ShareUtil {
         className: String? = null,
     ) {
         Log.i("startShare", "$packageName $className")
-        val shareIntent: Intent = Intent().apply {
-            if (uris.size == 1) {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_STREAM, uris.first())
-            } else {
-                action = Intent.ACTION_SEND_MULTIPLE
-                putParcelableArrayListExtra(Intent.EXTRA_STREAM, ArrayList(uris))
-            }
-            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-            addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
-            type = "image/*"
-            if (!packageName.isNullOrBlank() && !className.isNullOrBlank()) {
-                setClassName(packageName, className)
-            } else if (!packageName.isNullOrBlank()) {
-                setPackage(packageName)
-            }
+        val shareIntent = ShareCompat.IntentBuilder(context)
+            .apply { uris.forEach { addStream(it) } }
+            .setType("image/*")
+            .setChooserTitle(R.string.send_sticker)
+            .createChooserIntent()
+
+        if (!packageName.isNullOrBlank() && !className.isNullOrBlank()) {
+            shareIntent.setClassName(packageName, className)
+        } else if (!packageName.isNullOrBlank()) {
+            shareIntent.setPackage(packageName)
         }
-        context.startActivity(
-            Intent.createChooser(
-                shareIntent,
-                context.resources.getText(R.string.send_sticker)
-            )
-        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE && uris.size == 1) {
+            shareIntent.appendActions(context, uris)
+        }
+
+        context.startActivity(shareIntent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    private fun Intent.appendActions(
+        context: Context,
+        uris: List<Uri>
+    ) {
+        val actionIntents = ShareSheetAction.entries.map {
+            val intent = Intent(context, ShareSheetActionsReceiver::class.java).apply {
+                putExtra("action", it.ordinal)
+                this.setData(uris.first())
+            }
+
+            ChooserAction
+                .Builder(
+                    Icon.createWithResource(context, it.icon),
+                    context.getString(it.label),
+                    PendingIntent.getBroadcast(
+                        context, it.ordinal, intent,
+                        PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                    )
+                )
+                .build()
+        }.toTypedArray()
+        putExtra(Intent.EXTRA_CHOOSER_CUSTOM_ACTIONS, actionIntents)
     }
 }
